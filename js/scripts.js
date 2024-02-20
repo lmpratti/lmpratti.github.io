25a7fe74a3634b10750fca580e956f597aa62363
lathampratti <lathmadhuri.pratti@iiitb.org>
2024-02-20 11:48:39 +1030

    Update image visibility to 3seconds

--- js/scripts.js
+++ js/scripts.js
@@ -32,5 +32,5 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
